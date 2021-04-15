44b6e8924de758b786a56f9555cb74b4b175153a
rosemaryto <rosemary_to@ymail.com>
2021-04-14 21:22:55 -0700

    1-10

--- beginning/js/01-arrays-practice.js
+++ beginning/js/01-arrays-practice.js
@@ -1,22 +1,77 @@
 //STEP 1
+// let arrMovies = ["Clueless", "Point Break", "Speed", "The Mummy", "Enola"];
+// console.log(arrMovies[1]);
 
 //STEP 2
+// let movies = new Array(5);
+// movies[0] = "Clueless";
+// movies[1] = "Point Break";
+// movies[2] = "Speed";
+// movies[3] = "The Mummy";
+// movies[4] = "Enola";
+// console.log(movies[0]);
+
 
 //STEP 3
+// let movies = new Array(5);
+// movies[0] = "Clueless";
+// movies[1] = "Point Break";
+// movies[2] = "Moxie";
+// movies[3] = "Speed";
+// movies[4] = "The Mummy";
+// movies[5] = "Enola";
+// console.log(movies.length);
 
 //STEP 4
+// let movies = ["Clueless", "Point Break", "Speed", "The Mummy", "Enola"];
+// delete movies[0];
+// console.log(movies);
 
 //STEP 5
+// let movies = ["Clueless", "Point Break", "Speed", "The Mummy", "Enola", "Moxie", "The Matrix"];
+// for (movie in movies) {
+//     console.log(movies[movie]);
+// }
 
 //STEP 6
+// const movies = ["Clueless", "Point Break", "Speed", "The Mummy", "Enola", "Moxie", "The Matrix"];
+// for (const movie of movies) {
+//     console.log(movie);
+// }
 
 //STEP 7
+// let movies = ["Clueless", "Point Break", "Speed", "The Mummy", "Enola", "Moxie", "The Matrix"];
+// for (movie in movies) {
+//     console.log(movies.sort()[movie]);
+// }
 
 //STEP 8
+// let movies = ["Clueless", "Point Break", "Speed", "The Mummy", "Enola", "Moxie", "The Matrix"];
+// let leastFavMovies = ["The Flinstones", "What Lies Below", "Magic Mike"];
+// console.log("Movies I like:");
+// for (let i = 0; i < movies.length; i++) {
+//     console.log(movies[i] + "\n");
+// }
+// console.log("Movies I regret watching:");
+// for (let i = 0; i < leastFavMovies.length; i++) {
+//     console.log(leastFavMovies[i] + "\n");
+// }
+
 
 //STEP 9
+// let moviess = ["Clueless", "Point Break", "Speed", "The Mummy", "Enola", "Moxie", "The Matrix"];
+// let leastFavMovies = ["The Flinstones", "What Lies Below", "Magic Mike"];
+// movies = moviess.concat(leastFavMovies);
+// movies.reverse();
+// console.log(movies.toString());
 
 //STEP 10
+// let moviess = ["Clueless", "Point Break", "Speed", "The Mummy", "Enola", "Moxie", "The Matrix"];
+// let leastFavMovies = ["The Flinstones", "What Lies Below", "Magic Mike"];
+// movies = moviess.concat(leastFavMovies);
+// movies.reverse();
+// console.log(movies.toString());
+// console.log(movies[9]);
 
 //STEP 11
 
